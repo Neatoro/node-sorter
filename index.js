99bca05e31724d97fa416e2d3d26091e695fdaa9
Moritz Schramm <moritz.schramm@gmx.de>
2018-07-13 17:22:22 +0200

    Add main file

---
+++ index.js
@@ -0,0 +1,13 @@
+const bubblesort = require('./lib/bubblesort');
+const heapsort = require('./lib/heapsort');
+const insertionsort = require('./lib/insertionsort');
+const mergesort = require('./lib/mergesort');
+const quicksort = require('./lib/quicksort');
+
+module.exports = {
+    bubblesort,
+    heapsort,
+    insertionsort,
+    mergesort,
+    quicksort
+};
